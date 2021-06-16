ddf75cc7818a01cc818a27fa77e671f7fcebac77
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-06-15 23:44:10 -0300

    Initial commit

---
+++ 5-AtribuiçoesDeVariaveis.js
@@ -0,0 +1,13 @@
+console.log('Trabalhando com variaveis');
+// JS e case sensitive
+
+const idade = 29;
+const nome = 'Tássio';
+
+
+console.log(idade);
+console.log(nome);
+console.log(ano);
+
+// Não fazer
+//mes = "Janeiro"
